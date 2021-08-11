5c7ecb81724fb181d43761a8f8755b8fffacaa03
rrtdcl <hermit965@gmail.com>
2021-08-11 14:43:04 -0700

    added Lists for appointments

--- app/src/main/java/com/example/cscb07summer/ui/login/SpecializationActivity.java
+++ app/src/main/java/com/example/cscb07summer/ui/login/SpecializationActivity.java
@@ -13,6 +13,9 @@ import com.example.cscb07summer.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Collections;
+import java.util.List;
+
 
 public class SpecializationActivity extends AppCompatActivity {
 
@@ -27,7 +30,6 @@ public class SpecializationActivity extends AppCompatActivity {
         EditText spEdit = (EditText) findViewById(R.id.register_sp);
         String sp = spEdit.getText().toString();
         Intent intent = getIntent();
-        String uid = intent.getStringExtra("uid");
         if(sp.isEmpty()){
             Toast.makeText(SpecializationActivity.this, "please enter specialization",
                     Toast.LENGTH_SHORT).show();
@@ -37,13 +39,26 @@ public class SpecializationActivity extends AppCompatActivity {
             String gender = intent.getStringExtra("gender");
             String password = intent.getStringExtra("password");
             String name = intent.getStringExtra("name");
-            ref.child("Doctors").child(uid).setValue(uid);
-            ref.child("Doctors").child(uid).child("Birth date").setValue(birthDate);
-            ref.child("Doctors").child(uid).child("Email").setValue(email);
-            ref.child("Doctors").child(uid).child("Gender").setValue(gender);
-            ref.child("Doctors").child(uid).child("Password").setValue(password);
-            ref.child("Doctors").child(uid).child("Name").setValue(name);
-            ref.child("Doctors").child(uid).child("Specialization").setValue(sp);
+
+            ref.child("Doctors").child(email).setValue(email);
+            ref.child("Doctors").child(email).child("Birth date").setValue(birthDate);
+            ref.child("Doctors").child(email).child("Email").setValue(email);
+            ref.child("Doctors").child(email).child("Gender").setValue(gender);
+            ref.child("Doctors").child(email).child("Password").setValue(password);
+            ref.child("Doctors").child(email).child("Name").setValue(name);
+            ref.child("Doctors").child(email).child("Specialization").setValue(sp);
+            List<String> emptyList = Collections.<String>emptyList();
+            emptyList.add("Previous Patients");
+            ref.child("Doctors").child(email).child("Previous Patients").setValue(emptyList);
+            emptyList.clear();
+            emptyList.add("Upcoming Appointments");
+            ref.child("Doctors").child(email).child("Upcoming Appointments").setValue(emptyList);
+            emptyList.clear();
+            emptyList.add("Past Appointments");
+            ref.child("Doctors").child(email).child("Past Appointments").setValue(emptyList);
+            emptyList.clear();
+            emptyList.add("Availability");
+            ref.child("Doctors").child(email).child("Availability").setValue(emptyList);
             Toast.makeText(SpecializationActivity.this, "register success",
                     Toast.LENGTH_SHORT).show();
             intent = new Intent(this, LoginActivity.class);
