bba0f4610359a8269b5abb31adff693922386edf
Wader <waderzheng12345@gmail.com>
2021-08-11 13:05:32 -0400

    Doctor Profile now properly display users information and other changes.

--- app/src/main/java/com/example/cscb07summer/profile/DoctorMain.java
+++ app/src/main/java/com/example/cscb07summer/profile/DoctorMain.java
@@ -2,19 +2,26 @@ package com.example.cscb07summer.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cscb07summer.R;
 import com.example.cscb07summer.ui.login.LoginActivity;
 import com.example.cscb07summer.ui.login.register;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class DoctorMain extends AppCompatActivity{
@@ -23,19 +30,25 @@ public class DoctorMain extends AppCompatActivity{
     private Button patientList;
     private Button appointments;
     private Button logout;
-
     private Button patientLink;
+    private FirebaseAuth mAuth;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doctor_main_interface);
 
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("Username");
+
         profile = (Button)findViewById(R.id.ProfileButton);
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openProfile();
+                openProfile(username);
             }
         });
 
@@ -43,7 +56,7 @@ public class DoctorMain extends AppCompatActivity{
         patientList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openPatientList();
+                openPatientList(username);
             }
         });
 
@@ -51,7 +64,7 @@ public class DoctorMain extends AppCompatActivity{
         appointments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openAppointments();
+                openAppointments(username);
             }
         });
 
@@ -72,16 +85,19 @@ public class DoctorMain extends AppCompatActivity{
         });
     }
 
-    public void openProfile() {
+    public void openProfile(String username) {
         Intent intent = new Intent(this, DoctorProfile.class);
+        intent.putExtra("Username", username);
         startActivity(intent);
     }
-    public void openPatientList() {
+    public void openPatientList(String username) {
         Intent intent = new Intent(this, DoctorPatientList.class);
+        intent.putExtra("Username", username);
         startActivity(intent);
     }
-    public void openAppointments() {
+    public void openAppointments(String username) {
         Intent intent = new Intent(this, DoctorAppointment.class);
+        intent.putExtra("Username", username);
         startActivity(intent);
     }
     public void openPatientProfile(){
