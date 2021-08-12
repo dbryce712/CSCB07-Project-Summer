70b2bcc4e75b1e1bdbfcefbb98501ca011ac4a74
Wader <waderzheng12345@gmail.com>
2021-08-11 23:33:05 -0400

    Minor Changes.

--- app/src/main/java/com/example/cscb07summer/profile/PatientDoctorList.java
+++ app/src/main/java/com/example/cscb07summer/profile/PatientDoctorList.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cscb07summer.R;
 
 public class PatientDoctorList extends AppCompatActivity {
+
+    private Button one, two, three, four, five, six, seven, eight, nine, ten;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,89 @@ public class PatientDoctorList extends AppCompatActivity {
         Intent intent = getIntent();
         String username = intent.getStringExtra("Username");
 
+        one = (Button)findViewById(R.id.button2);
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        two = (Button)findViewById(R.id.button3);
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        three = (Button)findViewById(R.id.button4);
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        four = (Button)findViewById(R.id.button5);
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        five = (Button)findViewById(R.id.button6);
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        six = (Button)findViewById(R.id.button7);
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        seven = (Button)findViewById(R.id.button8);
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        eight = (Button)findViewById(R.id.button9);
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        nine = (Button)findViewById(R.id.button10);
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+        ten = (Button)findViewById(R.id.button11);
+        ten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBook();
+            }
+        });
+
+    }
+    public void openBook() {
+        Intent intent = new Intent(this, BookAppointment.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cscb07summer/profile/ProfilePatient.java
+++ app/src/main/java/com/example/cscb07summer/profile/ProfilePatient.java
@@ -7,23 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cscb07summer.R;
 import com.example.cscb07summer.ui.login.LoginActivity;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.jetbrains.annotations.NotNull;
 
 public class ProfilePatient extends AppCompatActivity {
 
@@ -95,7 +90,7 @@ public class ProfilePatient extends AppCompatActivity {
             }
         });
 
-        book = (Button)findViewById(R.id.BookAppointmentButton);
+        book = (Button)findViewById(R.id.ViewAppointmentButton);
         list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
