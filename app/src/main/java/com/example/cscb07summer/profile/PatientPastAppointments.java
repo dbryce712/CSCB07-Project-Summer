8695235ff1f1e3d5e14a6fa38526ab7d1799364d
Firesword05 <firesword05@gmail.com>
2021-08-11 23:46:42 -0400

    added Patient viewAppointments page

---
+++ app/src/main/java/com/example/cscb07summer/profile/PatientPastAppointments.java
@@ -0,0 +1,17 @@
+package com.example.cscb07summer.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cscb07summer.R;
+
+public class PatientPastAppointments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_past_appointments);
+
+    }
+}
